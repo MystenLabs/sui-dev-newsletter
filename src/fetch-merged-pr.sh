6c77a07409bb16094b4e962870842cf95bd2f2f8
Stefan Stanciulescu <stefan.stanciulescu@mystenlabs.com>
2023-10-30 21:05:02 -0700

    Switch to fetching the PRs from mystenlabs/sui and not the whole ML organization

--- src/fetch-merged-pr.sh
+++ src/fetch-merged-pr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #gh pr list --repo mystenlabs/sui -L 5000 --state merged --search merged:$1..$2 --json title --json url | jq -r '.[] | "* [" + .title + "]" + "(" + .url + ")" ' | sort
-gh search prs --owner mystenlabs -L 1000 --merged --merged-at $1..$2 --json title --json url | jq -r '.[] | "* [" + .title + "]" + "(" + .url + ")" ' | sort
+gh search prs --repo=mystenlabs/sui -L 1000 --merged --merged-at $1..$2 --json title --json url | jq -r '.[] | "* [" + .title + "]" + "(" + .url + ")" ' | sort
